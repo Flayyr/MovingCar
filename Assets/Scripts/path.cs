0081ccb6806c69489206c0e92a0916e506e2a98e
Deanna <deannayee0108@gmail.com>
2019-08-23 15:56:25 -0700

    Initial commit

---
+++ Assets/Scripts/path.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class path : MonoBehaviour {
+    public Transform[] points;
+    private int currentWayPoint = 0;
+    Transform targetWayPoint;
+
+    public float speed = 4f;
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+        if(currentWayPoint < this.points.Length) {
+            if(targetWayPoint == null) {
+                targetWayPoint = points[currentWayPoint];
+            }
+            drive();
+        }
+    }
+
+    private void drive() {
+        transform.forward = Vector3.RotateTowards(transform.forward, targetWayPoint.position - transform.position, speed * Time.deltaTime, 0.0f);
+        transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position, speed * Time.deltaTime);
+        if(transform.position == targetWayPoint.position) {
+            currentWayPoint++;
+            if(currentWayPoint < this.points.Length) {
+                targetWayPoint = points[currentWayPoint];
+            }
+        }
+    }
+}
